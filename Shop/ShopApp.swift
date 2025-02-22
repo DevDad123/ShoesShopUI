87b218e1eed3463ad830537b22f6417ff090e835
Dev <quanganhng14@gmail.com>
2025-02-22 21:32:00 +0700

    Initial Commit

---
+++ Shop/ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopApp.swift
+//  Shop
+//
+//  Created by Quang Anh Nguyen on 22/2/25.
+//
+
+import SwiftUI
+
+@main
+struct ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
